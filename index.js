5ffdae9d28a066480e9f485c776dfb6d6cfa09de
Jeong Pak <dannypak716@gmail.com>
2021-08-11 12:35:31 -0400

    completed up to and including task 4

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(originalFlavors){
+  return originalFlavors;
 }    
 
 
@@ -64,8 +64,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(originalFlavors){
+  if(originalFlavors.length === 31){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -81,8 +85,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(originalFlavor){
+  originalFlavor.unshift('Rainbow Sherbert');
+  return originalFlavor;
 }
 
 
@@ -97,8 +102,9 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(originalFlavor){
+  originalFlavor.pop();
+  return originalFlavor;
 }
 
 
